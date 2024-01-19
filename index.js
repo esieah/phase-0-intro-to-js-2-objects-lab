bfcd8f179498c36baf5f19db953bcffc00a055de
Emesiana Celestine <celelove4all@gmail.com>
2024-01-19 04:53:16 +0100

    phase-0-intro-to-js-2-objects-lab

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution in this file!
+const employee = {name: "sun", strretAddress: "21 michaelstreet"}
+
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+
+  const newEmployee = {...employee};
+
+  newEmployee[key] = value;
+  return newEmployee
+
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+  employee[key] = value;
+  return employee
+}
+
+
+function deleteFromEmployeeByKey(employee, key){
+  const newEmployee = {employee}
+   delete newEmployee.key
+
+   return newEmployee
+
+}
+
+
+
+
+
+
+function destructivelyDeleteFromEmployeeByKey(employee, name){
+    delete employee.name
+    return employee
+}
